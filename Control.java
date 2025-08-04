ac2d301c6f9894853a5f5f3a0a39d065f5ef4df6
lathiyaom <lathiya52725@gmail.com>
2025-08-04 06:34:33 +0530

    second

--- Control.java
+++ Control.java
@@ -5,6 +5,16 @@ public class Control {
         System.out.println(NoOfGcd());
     }
 
+    public static int GCD(int A, int B) {
+        for (; A != 0; ) {
+            int A_ = B % A;
+            int B_ = A;
+            A = A_;
+            B = B_;
+        }
+        return B;
+    }
+
     public static int NoOfGcd() {
         Scanner sc = new Scanner(System.in);
         int ans = sc.nextInt();
