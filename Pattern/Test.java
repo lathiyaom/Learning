9ec52a6fb2396463d867afdc4c29692320b7fcf1
lathiyaom <lathiya52725@gmail.com>
2025-08-04 05:55:08 +0530

    first commit in fatuers

--- Pattern/Test.java
+++ Pattern/Test.java
@@ -11,26 +11,5 @@ public class Test {
 6 6 6 6 6 6
         *
         * */
-
-
-        int n = 6;
-        for (int i = 1; i <= n; i++) {
-            for (int s = 1; s <= n - i; s++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= (2 * i) - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int i = n - 1; i >= 1; i--) {
-            for (int s = 1; s <= n - i; s++) {
-                System.out.print(" ");
-            }
-            for (int j = (2 * i) - 1; j >= 1; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
