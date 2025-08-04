8d481b663fca9c1118afbba2316744fda2802492
lathiyaom <lathiya52725@gmail.com>
2025-08-04 06:18:30 +0530

    xyz

--- Pattern/Test.java
+++ Pattern/Test.java
@@ -23,14 +23,5 @@ public class Test {
             }
             System.out.println();
         }
-        for (int i = n - 1; i >= 1; i--) {
-            for (int s = 1; s <= n - i; s++) {
-                System.out.print(" ");
-            }
-            for (int j = (2 * i) - 1; j >= 1; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
