b69f5f080b1b0fe8bac84ab380d44f5d09aac81b
lathiyaom <lathiya52725@gmail.com>
2025-08-04 06:39:56 +0530

    okok

--- Control.java
+++ Control.java
@@ -5,25 +5,5 @@ public class Control {
         System.out.println(NoOfGcd());
     }
 
-    public static int GCD(int A, int B) {
-        for (; A != 0; ) {
-            int A_ = B % A;
-            int B_ = A;
-            A = A_;
-            B = B_;
-        }
-        return B;
-    }
-
-    public static int NoOfGcd() {
-        Scanner sc = new Scanner(System.in);
-        int ans = sc.nextInt();
-
-        for (int i = 1; i <= 2; i++) {
-            ans = GCD(ans, sc.nextInt());
-
-        }
-        return ans;
-
     }
 }
